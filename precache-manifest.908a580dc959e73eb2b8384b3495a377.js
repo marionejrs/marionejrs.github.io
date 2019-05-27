048cdde82fb349d94153e7295c43a2146709f60e
iequivocality <xanthiandere@gmail.com>
2019-05-27 12:45:10 +0800

    Updates

---
+++ precache-manifest.908a580dc959e73eb2b8384b3495a377.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613464e5ab81018ec47737a107c418bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f80a1d60f5cc1e659938",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f80a1d60f5cc1e659938",
+    "url": "/static/js/main.2ded8402.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
