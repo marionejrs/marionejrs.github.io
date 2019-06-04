1e95882dd7888b65d5c1c16b7f25d7eabd0bd9bb
marionejrs <marionejr.semilla@gmail.com>
2019-06-04 21:14:34 +0800

    adjusted position of contact email, added transition for email

--- src/contact/ContactEmail.tsx
+++ src/contact/ContactEmail.tsx
@@ -7,6 +7,9 @@ export default class ContactEmail extends Component<ContactEmailProps> {
         return (
             <div className={'contact-container'}>
                 <a href={`mailto:${this.props.email}`}>{this.props.email}</a>
+                <svg width="150" height="5" className="item-bottom-border">
+                    <line x1="0" y1="2" x2="150" y2="2" style={{ stroke: "#391552", strokeWidth: 1}} />
+                </svg>
             </div>
         );
     }
